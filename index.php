6f615763629db78676fb236e833a2a39838663b0
FrancoCaneti <CAnetifranco95@hmail.com>
2021-06-02 10:51:05 +0200

    index

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/*  
+-   Crea e modellizza classi per gestire i prodotti di uno shop;
+    ad esempio, ci saranno sicuramente dei prodotti da acquistare
+     e degli utenti che fanno shopping.
+
+-   Strutturare le classi gestendo l'ereditarietà dove 
+    necessario; ad esempio ci potrebbero essere degli utenti 
+    premium che hanno diritto a degli sconti esclusivi, oppure 
+    diverse tipologie di prodotti.
+
+-   Eseguire poi degli output istanziando oggetti delle varie 
+    classi.
+
+BONUS
+Gestite eventuali eccezioni (exception) che si possono verificare 
+(es: parametro errato passato a un metodo per settare l'età di 
+un utente).
+*/
